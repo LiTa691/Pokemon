60007d8131a53651fade61de1f5d9e1a2d0b7372
LiTa691 <78980502+LiTa691@users.noreply.github.com>
2024-04-23 22:54:38 +0200

    get Base States

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function renderPokemonInfo() {
     document.getElementById('pokemonNumber').innerHTML = currentPokemon['id'];
     document.getElementById('pokemonType').innerHTML = currentPokemon['types']['0']['type']['name'];
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['home']['front_default'];
+    document.getElementById('baseStateHp').innerHTML = currentPokemon['stats']['0']['stat']['name'];
+    document.getElementById('baseStateHpNumber').innerHTML = currentPokemon['stats']['0']['base_stat'];
+    document.getElementById('baseStateAttack').innerHTML = currentPokemon['stats']['1']['stat']['name'];
+    document.getElementById('baseStateAttackNumber').innerHTML = currentPokemon['stats']['1']['base_stat'];
+    document.getElementById('baseStateDefense').innerHTML = currentPokemon['stats']['2']['stat']['name'];
+    document.getElementById('baseStateDEfenseNumber').innerHTML = currentPokemon['stats']['2']['base_stat'];
+    document.getElementById('baseStateSpAtk').innerHTML = currentPokemon['stats']['3']['stat']['name'];
+    document.getElementById('baseStatespAtkNumber').innerHTML = currentPokemon['stats']['3']['base_stat'];
+    document.getElementById('baseStateSpDef').innerHTML = currentPokemon['stats']['4']['stat']['name'];
+    document.getElementById('baseStateSpDefNumber').innerHTML = currentPokemon['stats']['4']['base_stat'];
 }
